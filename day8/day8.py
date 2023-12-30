67ea14fb5646e44018aeccf6a98826a032230f56
Neil Chazin <nchazin@salesforce.com>
2023-12-29 20:15:44 -0800

    day part 1

--- day8/day8.py
+++ day8/day8.py
@@ -10,16 +10,37 @@ class Node:
     def __init__(self, left, right):
         self.left = left
         self.right = right
+    
+    def __repr__(self):
+        return f"L:{self.left} R:{self.right}"
 
 nodemap = {}
 
-for row in rows[3:]:
+for row in rows[2:]:
     source, dest = row.strip().split('=')
     source = source.strip()
     dest = dest.replace('(', '').replace(')','').replace(' ','')
     left, right = dest.split(',')
     nodemap[source] = Node(left,right)
 
-breakpoint()
-print(nodemap)
     
+curlabel = 'AAA'
+count = 0
+
+
+lastnode = set()
+
+for i in range(100000):
+    for d in directions:
+        if curlabel == 'ZZZ':
+            break
+        curnode = nodemap[curlabel]
+        if d == 'L':
+            curlabel = curnode.left
+        elif d == 'R':
+            curlabel = curnode.right
+        count += 1
+    if curlabel == 'ZZZ':
+        break
+
+print(count)
